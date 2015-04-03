05f00047aa5db0f86000941eb873fe7369fbe29b
Arvind <arvindsriv@gmail.com>
2015-04-04 00:01:22 +0530

    Initially checking code to Git
    Initially checking code to Git

---
+++ Models/Supplier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagement.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string SupplierName { get; set; }
+        public string Address { get; set; }
+        public string ContactPerson { get; set; }
+        public string ContactNumber { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool IsValid { get; set; }
+        
+    }
+}
